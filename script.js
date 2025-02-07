427e55c799e79b40c2909f5fb55d214ba6a508c3
sauz-zeth <sauzzeth@vk.com>
2025-02-07 21:06:43 +0300

    added LocalStorage, formatNumber(), deleteSymbol() + small changes

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 const u_display = document.getElementById("equation")
 const d_display = document.getElementById("answer")
 
+window.addEventListener("load", () => {
+    const u_display_saved = localStorage.getItem("u_display_value");
+    const d_display_saved = localStorage.getItem("d_display_value");
+
+    if (u_display_saved !== null) {
+      u_display.value = u_display_saved;
+    }
+    if (d_display_saved !== null) {
+      d_display.value = d_display_saved;
+    }
+  });
+
+  function saveToLocalStorage() {
+    localStorage.setItem("u_display_value", u_display.value);
+    localStorage.setItem("d_display_value", d_display.value);
+  }
+
+function appendToDisplay(input) {
+    d_display.value += input;
+    saveToLocalStorage();
+}
+
+function clearDisplay() {
+    u_display.value = "";
+    d_display.value = "";
+    saveToLocalStorage();
+}
+
+function deleteSymbol() {
+    d_display.value = d_display.value.slice(0, -1);
+    saveToLocalStorage();
+}
+
+function formatNumber(num) {
+    if (Math.abs(num) < 1e-5 || Math.abs(num) >= 1e6) {
+        return num.toExponential(5);
+    } else {
+        return parseFloat(num.toPrecision(8)).toString();
+    }
+}
+
 function stringify(display_value) {
     let s = String(display_value);
     s = s.replace('÷', '/');
@@ -8,35 +49,15 @@ function stringify(display_value) {
     return s
 }
 
-function last_element_index(s) {
-    const symbols = "+-/%";
-    for (const c of s) {
-        if(symbols.includes(c)) {
-            return s.indexOf(c);
-        }
-    }
-    return 0;
-}
-
-function appendToDisplay(input) {
-    d_display.value += input;
-}
-
-function clearDisplay() {
-    u_display.value = "";
-    d_display.value = "";
-}
-
-function changeSign() {
-    s = stringify(d_display.value);
-    a = last_element_index(s);
-    console.log(a);
-
-}
-
 function calculate() {
     s = stringify(d_display.value)
 
-    u_display.value = d_display.value;
-    d_display.value = eval(s);
+    dvalue = d_display.value;
+    try {
+        d_display.value = formatNumber(eval(s));
+        u_display.value = dvalue;
+    } catch (error) {
+       u_display.value = 'error'; 
+    }
+    saveToLocalStorage();
 }
