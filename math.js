d7cc932f45d635e05c5508afbc84ba0a9fa5a75f
sauz-zeth <sauzzeth@vk.com>
2025-02-08 16:50:41 +0300

    deleted eval(), improved rounding, better scrolling for input

---
+++ math.js
@@ -0,0 +1,72 @@
+function math_calc(expression) {
+    function operate(operand1, operand2, operator) {
+        switch (operator) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                if (operand2 !== 0) {
+                    return operand1 / operand2;
+                } else {
+                    throw new Error("Division by zero is not allowed.");
+                }
+            case '%':
+                return operand1 % operand2;
+            default:
+                throw new Error("Unsupported operator: " + operator);
+        }
+    }
+
+    function parse(expression) {
+        expression = expression.replace(/\s+/g, '');
+
+        let numbers = [];
+        let operators = [];
+        let num = '';
+
+        for (let i = 0; i < expression.length; i++) {
+            const char = expression[i];
+
+            if (/\d|\./.test(char)) {
+                num += char;
+            } else {
+                if (num) {
+                    numbers.push(parseFloat(num));
+                    num = '';
+                }
+                operators.push(char);
+            }
+        }
+
+        if (num) {
+            numbers.push(parseFloat(num));
+        }
+
+        return { numbers, operators };
+    }
+
+    function evaluate(numbers, operators) {
+        for (let i = 0; i < operators.length; i++) {
+            if (operators[i] === '*' || operators[i] === '/' || operators[i] === '%') {
+                const result = operate(numbers[i], numbers[i + 1], operators[i]);
+                numbers[i] = result;
+                numbers.splice(i + 1, 1);
+                operators.splice(i, 1);
+                i--;
+            }
+        }
+
+        let result = numbers[0];
+        for (let i = 0; i < operators.length; i++) {
+            result = operate(result, numbers[i + 1], operators[i]);
+        }
+
+        return result;
+    }
+
+    const { numbers, operators } = parse(expression);
+    return evaluate(numbers, operators);
+}
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const u_display = document.getElementById("equation")
 const d_display = document.getElementById("answer")
 const operators = "+-×÷%";
+const display_size = 10;
 
 const display = {
     equationElement: document.getElementById("equation"),
@@ -38,6 +39,7 @@ function appendToDisplay(input) {
     else {
         d_display.value += input;
     }
+    d_display.scrollLeft = d_display.scrollWidth - d_display.clientWidth;
     saveToLocalStorage();
 }
 
@@ -53,12 +55,16 @@ function deleteSymbol() {
 }
 
 function formatNumber(num) {
-    if (num == 0) {return num};
-    if (Math.abs(num) < 1e-5 || Math.abs(num) >= 1e6) {
-        return num.toExponential(5);
-    } else {
-        return parseFloat(num.toPrecision(7)).toString();
+    const l = String(num).length;
+    if(String(num).length > display_size) {
+        if(num >= 10 ** display_size) {
+            return num.toExponential(display_size - 4 - String(l).length);
+        }
+        else {
+            return num.toPrecision(display_size - 3);
+        }
     }
+    return num;
 }
 
 function stringify(display_value) {
@@ -74,7 +80,7 @@ function calculate() {
 
     dvalue = d_display.value;
     try {
-        d_display.value = formatNumber(eval(s));
+        d_display.value = formatNumber(math_calc(s));
         u_display.value = dvalue;
     } catch (error) {
        u_display.value = 'error'; 
@@ -87,14 +93,3 @@ document.addEventListener('keydown', function(event) {
         calculate()
     }
 });
-
-//TODO: функцию, чтобы можно было ввести только один знак
-//TODO: добавить : в реплейс
-
-//TDOD: написать в ридми что есть рукописный ввод,
-// и что есть функция одного знака
-
-//TODO: исправить код
-// написать что процент выполняет функцию остатка от деления как в калькуляторе от apple
-
-//TODO: сделать выполнение calculate на enter
